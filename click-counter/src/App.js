07f3c70df9dedd74dcca052bda3344c68af6ed82
Lucas Polizeli <lumpolizeli@gmail.com>
2019-06-12 15:44:14 -0300

    Implemente second module

---
+++ click-counter/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      counter: 0
+    };
+  }
+  render() {
+    return (
+      <div data-test="component-app">
+        <h1 data-test="counter-display">The counter is currently</h1>
+        <button data-test="increment-button">Increment</button>
+      </div>
+    );
+  }
+}
+
+export default App;
